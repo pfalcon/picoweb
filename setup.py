9e6f7a5887673d96daa4bf93195d3ec87e76a17e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-21 22:11:10 +0300

    Release 0.4.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(name='picoweb',
-      version='0.3',
+      version='0.4',
       description="""A very lightweight, memory-efficient async web framework
 for MicroPython.org and its asyncio_micro.""",
       url='https://github.com/pfalcon/picoweb',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
       license='MIT',
-      py_modules=['picoweb'],
-      install_requires=['micropython-asyncio_micro'])
+      packages=['picoweb'],
+      install_requires=['micropython-asyncio_micro', 'utemplate'])
