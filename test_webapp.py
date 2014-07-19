55c2a813a0bdfe1be78596dbfe6c13b8aed176df
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-07-19 21:24:43 +0300

    test_webapp.py: Use start_response function.

--- test_webapp.py
+++ test_webapp.py
@@ -9,9 +9,7 @@ def index(writer, req):
     yield from writer.awrite("I can show you a table of <a href='squares'>squares</a>.")
 
 def squares(writer, req):
-    yield from writer.awrite("HTTP/1.0 200 OK\r\n")
-    yield from writer.awrite("Content-Type: text/html\r\n")
-    yield from writer.awrite("\r\n")
+    yield from picoweb.start_response(writer)
     yield from picoweb.render(writer, "squares", (req,))
 
 
