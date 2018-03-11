948bbafca6112ff7883eb8ec7113bd09d127cce6
Eric Pascual <eric@pobot.org>
2018-03-11 17:54:48 +0100

    bump version 1.4.1 -> 1.4.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sdist_upip
 
 
 setup(name='picoweb',
-      version='1.4.1',
+      version='1.4.2',
       description="A very lightweight, memory-efficient async web framework \
 for MicroPython.org and its uasyncio module.",
       long_description=open('README.rst').read(),
