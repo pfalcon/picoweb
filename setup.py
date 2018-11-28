e680d0a8a7ebeaca4872b5cbdc204422589fd810
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-28 21:46:41 +0300

    setup: Add comment about lack of dependency on ulogging.

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ for MicroPython and its uasyncio module.",
       cmdclass={'sdist': sdist_upip.sdist},
       packages=['picoweb'],
       # Note: no explicit dependency on 'utemplate', if a specific app uses
-      # templates, it must depend on it.
+      # templates, it must depend on it. Likewise, don't depend on
+      # micropython-ulogging as application might not use logging.
       install_requires=['micropython-uasyncio', 'micropython-pkg_resources'])
