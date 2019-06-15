fb4354dc601e81ebc37c4c08b5df342f0833d305
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-06-15 23:32:55 +0300

    picoweb: Change default content-type to "text/html; charset=utf-8".
    To let MicroPython's utf-8 unicode strings to work out of the box in browser
    HTML output, explicitly set output encoding as such.

--- picoweb/__init__.py
+++ picoweb/__init__.py
@@ -39,7 +39,7 @@ def jsonify(writer, dict):
     yield from start_response(writer, "application/json")
     yield from writer.awrite(ujson.dumps(dict))
 
-def start_response(writer, content_type="text/html", status="200", headers=None):
+def start_response(writer, content_type="text/html; charset=utf-8", status="200", headers=None):
     yield from writer.awrite("HTTP/1.0 %s NA\r\n" % status)
     yield from writer.awrite("Content-Type: ")
     yield from writer.awrite(content_type)
